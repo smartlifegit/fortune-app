d45db57c4baf8ce571b04b87e1342f420a4f1b95
audrey <smartlife.ai@gmail.com>
2026-02-12 05:23:03 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'fortune-v1';
+const urlsToCache = ['fortune-app.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
